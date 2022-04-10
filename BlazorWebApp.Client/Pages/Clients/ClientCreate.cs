90f40b70848d105dd92e3a65f4e975b51b346010
jesus.jimenez <jesus.jimenez@mother.travel>
2022-04-09 21:05:44 -0500

    libs utils

--- BlazorWebApp.Client/Pages/Clients/ClientCreate.cs
+++ BlazorWebApp.Client/Pages/Clients/ClientCreate.cs
@@ -1,6 +1,32 @@
-﻿namespace BlazorWebApp.Client.Pages.Clients
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorWebApp.Client.Pages.Clients
 {
     public partial class ClientCreate
     {
+        [Inject] public IJSRuntime JsRuntime { get; set; }
+        private string DateSelect;
+
+        protected override async Task OnInitializedAsync()
+        {
+            //forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+            await JsRuntime.InvokeVoidAsync("Select2demo");
+        }
+
+        protected async Task InputMaskPhoneClick()
+        {
+            await JsRuntime.InvokeVoidAsync("InputPhoneMask");
+        }
+
+        protected async Task SelectDate()
+        {
+             await JsRuntime.InvokeVoidAsync("DateSelect");
+        }
+
+        protected async Task Select2Demo()
+        {
+            await JsRuntime.InvokeVoidAsync("Select2demo");
+        }
     }
 }
--- BlazorWebApp.Client/wwwroot/js/Clients/clientCreate.js
+++ BlazorWebApp.Client/wwwroot/js/Clients/clientCreate.js
@@ -1,1 +1,55 @@
-﻿
+﻿window.InputPhoneMask = () => {
+    $('.telphone').mask("(999) 999-9999").focusout(function(event) {
+        var target, phone, element;
+        target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+        phone = target.value.replace(/\D/g, '');
+        element = $(target);
+        element.unmask();
+        if (phone.length === 10) {
+            console.log(phone);
+            element.mask("(999) 999-9999");
+        }
+        else {
+            console.log("debe ser completo");
+        } 
+    });
+
+};
+
+window.DateSelect = () => {
+    $('#selectDate').datepicker({
+        format: "dd/mm/yyyy",
+        autoclose: true,
+        todayHighlight: true,
+        language:"es"
+    }).on('changeDate', function(e) {
+        console.log(e.format(0, "dd/mm/yyyy"));
+        $('#floatingDate').val(e.format(0, "dd/mm/yyyy"));
+    });
+
+};
+
+window.Select2demo = () => {
+    $('.js-example-basic-single').select2();
+};
+
+
+
+
+
+
+
+$.fn.datepicker.dates['es'] = {
+    days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    daysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sáb"],
+    daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+    months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+    today: "Hoy",
+    clear: "Borrar",
+    format: "dd/mm/yyyy",
+    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+    weekStart: 0
+};
+
+
